4108f12fb4be3b65342e7c72290a807e26949a74
Eason Lin <uu9924079@gmail.com>
2020-03-22 20:52:14 +0800

    init

--- README.md
+++ README.md
@@ -1,19 +1,3 @@
 # vue-table-excel
 
-## Project setup
-```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+參考 https://panjiachen.github.io/vue-element-admin-site/zh/feature/component/excel.html#%E5%8F%82%E6%95%B0 所實作的表格轉 Excel 練習
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,75 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h2>Input your new values here</h2>
+    <label>first name</label>
+    <input type="text" v-model="firstName" />
+    <label>last name</label>
+    <input type="text" v-model="lastName" />
+    <label>age</label>
+    <input type="number" v-model="age" />
+    <button @click="insert">Insert</button>
+    <br /><br /><br />
+
+    <button @click="downloadExcel">Download as .xlsx file</button>
+    <table style="width:100%">
+      <tr>
+        <th v-for="(value, index) in header">{{ value }}</th>
+      </tr>
+      <tr v-for="(item, index) in list" :key="index">
+        <td v-for="(value, index) in item" :key="index">
+          {{ value }}
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      age: '',
+
+      header: ['Firstname', 'Lastname', 'Age'],
+      list: [
+        ['Jill', 'Smith', 50],
+        ['Eve', 'Jackson', 94],
+        ['Jane', 'Doe', 26]
+      ]
+    }
+  },
+  methods: {
+    insert() {
+      if (!this.firstName || !this.lastName || !this.age) {
+        return alert('must input every column')
+      }
+      this.list.push([this.firstName, this.lastName, +this.age])
+      this.firstName = ''
+      this.lastName = ''
+      this.age = ''
+    },
+    downloadExcel() {
+      import('@/vendor/Export2Excel').then(excel => {
+        const tHeader = this.header
+        const data = this.list
+        excel.export_json_to_excel({
+          header: tHeader, //表头 必填
+          data, //具体数据 必填
+          filename: 'excel-list', //非必填
+          autoWidth: true, //非必填
+          bookType: 'xlsx' //非必填
+        })
+      })
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+table * {
+  border: 1px solid;
 }
 </style>
